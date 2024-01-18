9dd0b9991f1d9d9738c5934a33e2d97cae229eed
ClemRea <clementrea7@gmail.com>
2024-01-18 17:05:00 +0100

    add avatar + caroussel

--- my-app/src/components/homePage/Carrousel.jsx
+++ my-app/src/components/homePage/Carrousel.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import "../../style/homePage/caroussel.css";
 
 function Carrousel({ slides }) {
   const [current, setCurrent] = useState(0);
@@ -17,24 +18,37 @@ function Carrousel({ slides }) {
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
+
   return (
     <div className="avatar_container_flex">
       <div className="avatar_container">
-        <FontAwesomeIcon
-          icon={faCaretUp}
-          className="fleche fleche_gauche"
-          onClick={prevImage}
-        />
-        <img
-          src={slides[current]}
-          alt={`Avatar ${current}`}
-          className="avatar_img"
-        />
-        <FontAwesomeIcon
-          icon={faCaretUp}
-          className="fleche fleche_droite"
-          onClick={nextImage}
-        />
+        {current !== length - 1 && (
+          <FontAwesomeIcon
+            icon={faCaretUp}
+            className="fleche fleche_gauche"
+            onClick={prevImage}
+          />
+        )}
+        <figure className="avatar_figure">
+          {slides.map((slide, i) => (
+            <img
+              src={slide}
+              alt={`Avatar ${i}`}
+              className="avatar_img"
+              style={{
+                transform: `translateX(${-current * 182}px)`,
+              }}
+              key={i}
+            />
+          ))}
+        </figure>
+        {current !== 0 && (
+          <FontAwesomeIcon
+            icon={faCaretUp}
+            className="fleche fleche_droite"
+            onClick={nextImage}
+          />
+        )}
       </div>
     </div>
   );
--- my-app/src/components/homePage/playCard.jsx
+++ my-app/src/components/homePage/playCard.jsx
@@ -3,15 +3,15 @@ import HomePageBtn from "./btn";
 import JoinGameInput from "./joinGmameInput";
 import NameInput from "./nameInput";
 import Carrousel from "./Carrousel";
+import bibouz from "../../assets/bibouzPNG.png";
+import cassou from "../../assets/cassouPNG.png";
+import pirate from "../../assets/piratePNG.png";
+// import robot from "../../assets/robot.jpg";
 import "../../style/homePage/play_card.css";
 
 function PlayCard() {
   const AvatarCarrousel = () => {
-    const avatarImages = [
-      "../../assets/bibouzPNG",
-      "../../assets/piratePNG",
-      "../../assets/cassouPNG",
-    ];
+    const avatarImages = [pirate, bibouz, cassou];
     return <Carrousel slides={avatarImages} />;
   };
 
@@ -19,18 +19,6 @@ function PlayCard() {
     <div className="play_card_container">
       <div className="play_card">
         <h1 className="titre_h1">play</h1>
-        {/* <div className="avatar_container_flex">
-          <div className="avatar_container">
-            <FontAwesomeIcon
-              icon={faCaretUp}
-              className="fleche fleche_gauche"
-            />
-            <FontAwesomeIcon
-              icon={faCaretUp}
-              className="fleche fleche_droite"
-            />
-          </div>
-        </div> */}
         <AvatarCarrousel />
         <NameInput />
         <HomePageBtn />
